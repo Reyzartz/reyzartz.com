0adf564d0bcbd598894b73cfc6e502ff0a7eb59b
Reyzartz <reyanrahman@gmail.com>
2025-10-15 01:11:14 +0530

    added test html page

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
 	const filePath = path.join(__dirname, "index.html");
-	fs.streamFile(filePath, (err, stream) => {
+	fs.readFile(filePath, (err, data) => {
 		if (err) {
 			res.writeHead(404);
 			res.end('Not Found');
 		} else {
-			stream.pipe(res);
+			res.writeHead(200);
+			res.end(data);
 		}
-	});
+	})
 }).listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
